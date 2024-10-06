6dcd0374de926e4997830985cc51dfc95fb63807
Lejla <lejladzanko@gmail.com>
2024-10-06 14:29:03 +0200

    Change base name for vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/lejladzanko/lejla-portfolio-react',
+  base: '/lejla-portfolio-react/',
 })
